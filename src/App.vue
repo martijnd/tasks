7915210c2325b6e511b3aebda0dba192cee4ea09
Martijn Dorsman <martijn.dorsman@gmail.com>
2021-12-27 18:14:12 +0100

    MVP

--- src/App.vue
+++ src/App.vue
@@ -7,9 +7,26 @@
       <div v-else>
         <button @click="signOut">Sign out</button>
         <pre class="text-white">{{ user.displayName }}</pre>
-        <div v-if="tasks" v-for="task of tasks">
-          <Task :task="task" @update="() => updateTimestamp(task)" />
+        <div v-if="tasks" class="space-y-2">
+          <Task
+            v-for="task of tasks"
+            :task="task"
+            @update="() => updateTimestamp(task)"
+          />
         </div>
+        <form @submit.prevent="addTask" class="flex flex-col">
+          <input class="w-full" type="text" v-model="title" placeholder="Title" />
+          <input
+            class="w-full"
+            type="datetime-local"
+            v-model="timestamp"
+            placeholder="Datetime"
+          />
+          <button
+            class="bg-purple-600 rounded my-2 py-2 px-4 text-white font-semibold"
+            type="submit"
+          >Add</button>
+        </form>
       </div>
     </div>
   </div>
@@ -27,7 +44,7 @@ import {
   setPersistence,
   browserSessionPersistence
 } from 'firebase/auth'
-import { collection, doc, getDoc, getDocs, getFirestore, updateDoc, Timestamp, onSnapshot, query } from 'firebase/firestore';
+import { collection, doc, getDoc, getFirestore, updateDoc, Timestamp, onSnapshot, query, addDoc, getDocs } from 'firebase/firestore';
 import Task from './components/Task.vue';
 import { ITask } from './types/task';
 
@@ -52,6 +69,21 @@ async function updateTimestamp(task: ITask) {
     await updateDoc(docSnap.ref, {
       timestamp: Timestamp.now()
     });
+  }
+}
+
+const title = ref('');
+const timestamp = ref(new Date().toDateString());
+
+async function addTask() {
+  if (user.value) {
+
+    await addDoc(collection(db, 'users', user.value.uid, 'tasks'), {
+      title: title.value,
+      timestamp: Timestamp.fromDate(new Date(timestamp.value))
+    })
+
+    title.value = '';
   }
 }
 
